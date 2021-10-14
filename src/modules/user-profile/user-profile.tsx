11508c878b7d58b316f861e04b67918d70272d1c
EndlessHollow <marekklier.personal@gmail.com>
2021-10-14 22:02:52 +0200

    feat: add user-profile component

---
+++ src/modules/user-profile/user-profile.tsx
@@ -0,0 +1,48 @@
+import React, { FC } from "react";
+import styled from "styled-components";
+
+const pictureSize = "3rem;";
+
+const UserProfileContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: ${({ theme }) => theme.spacing[4]};
+`;
+
+const UserPhotoContainer = styled.div`
+  width: ${pictureSize};
+  height: ${pictureSize};
+`;
+
+const UserPhoto = styled.img`
+  display: block;
+  width: 100%;
+  height: 100%;
+  border-radius: 0.5rem;
+  object-position: top;
+  object-fit: cover;
+`;
+const Username = styled.span`
+  font-size: ${({ theme }) => theme.fontSize["text-base"]};
+  font-weight: ${({ theme }) => theme.fontWeight.fontMedium};
+  color: ${({ theme }) => theme.colors.darkBlue};
+`;
+
+export interface UserProfileProps {}
+
+export const UserProfile: FC<UserProfileProps> = (props): JSX.Element => {
+  return (
+    <UserProfileContainer>
+      <UserPhotoContainer>
+        <UserPhoto
+          src={"https://source.unsplash.com/mEZ3PoFGs_k/443×664"}
+          alt={"Profile Picture - Estella Horton"}
+          loading="lazy"
+        />
+      </UserPhotoContainer>
+      <Username>Estella Horton</Username>
+    </UserProfileContainer>
+  );
+};
+
+UserProfile.displayName = "User Profile";
