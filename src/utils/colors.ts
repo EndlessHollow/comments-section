6be454dd50a72c1f19e71117549cdca877514d37
EndlessHollow <marekklier.personal@gmail.com>
2021-10-14 22:01:15 +0200

    feat: add theme and global reset

---
+++ src/utils/colors.ts
@@ -0,0 +1,10 @@
+export type Colors = typeof colors;
+
+export const colors = {
+  black: "hsl(0, 100%, 0%)",
+  white: "hsl(0, 0%, 100%)",
+  grey: "hsl(222, 16%, 84%)",
+  lightgrey: "hsl(228, 33%, 97%)",
+  blue: "hsl(214, 99%, 57%)",
+  darkBlue: "hsl(215, 41%, 21%)",
+};
